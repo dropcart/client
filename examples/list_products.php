9f406e72ec5c7bee81b5c847a428d68aed0c3b8b
Praalhans <praalhans@users.noreply.github.com>
2016-12-02 10:33:41 +0100

    Test commit for Packagist.

--- examples/list_products.php
+++ examples/list_products.php
@@ -2,4 +2,9 @@
 
 // Authenticate with Dropcart. KEEP YOUR API KEY A SECRET!
 
+use Dropcart\Client;
+
+$client = new Client();
+$client->auth("<insert your API key here>");
+$list = $client->getProductListing();
 
--- module/dropcart.php
+++ module/dropcart.php
@@ -1,3 +1,22 @@
 <?php
 
+namespace Dropcart;
 
+class Client {
+	
+	public function auth($private_key)
+	{
+		
+	}
+	
+	public function getProductsListing()
+	{
+		
+	}
+	
+	public function getProductInfo()
+	{
+		
+	}
+	
+}
